ff7fa5a7a45624dfc8f65c113b49bfb9446f1e26
ricardo0129 <ruizr274@gmail.com>
2025-09-30 23:00:25 -0400

    working on my own middleware

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use axum::{Router, routing::get};
+use std::fmt;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 use std::time::Duration;
+use std::{future::Future, pin::Pin};
 use tokio::net::TcpListener;
+use tower::Service;
+use tower::ServiceBuilder;
 use tower_governor::{GovernorLayer, governor::GovernorConfigBuilder};
 
 use std::env;
@@ -10,7 +15,94 @@ use std::env;
 pub mod routes;
 use crate::routes::general_routes::{handler, handler_404, health_check, uptime};
 use crate::routes::{codeforces, github, leetcode, utils};
+use pin_project::pin_project;
+use tokio::time::Sleep;
+use tokio::time::sleep;
 use utils::AppState;
+
+#[derive(Debug, Default)]
+pub struct TimeoutError(());
+
+impl fmt::Display for TimeoutError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.pad("request timed out")
+    }
+}
+
+impl std::error::Error for TimeoutError {}
+pub type BoxError = Box<dyn std::error::Error + Send + Sync>;
+
+#[pin_project]
+pub struct ResponseFuture<F> {
+    #[pin]
+    response_future: F,
+    #[pin]
+    sleep: Sleep,
+}
+
+#[derive(Clone, Debug)]
+struct Timeout<S> {
+    inner: S,
+    timeout: Duration,
+}
+
+impl<S> Timeout<S> {
+    pub fn new(inner: S, timeout: Duration) -> Self {
+        Timeout { inner, timeout }
+    }
+}
+
+impl<F, Response, Error> Future for ResponseFuture<F>
+where
+    F: Future<Output = Result<Response, Error>>,
+    Error: Into<BoxError>,
+{
+    type Output = Result<Response, BoxError>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        match this.response_future.poll(cx) {
+            Poll::Ready(result) => {
+                let result = result.map_err(Into::into);
+                return Poll::Ready(result);
+            }
+            Poll::Pending => {}
+        }
+        match this.sleep.poll(cx) {
+            Poll::Ready(()) => {
+                let error = Box::new(TimeoutError(()));
+                return Poll::Ready(Err(error));
+            }
+            Poll::Pending => {}
+        }
+        Poll::Pending
+    }
+}
+
+impl<S, Request> Service<Request> for Timeout<S>
+where
+    S: Service<Request>,
+    S::Error: Into<BoxError>,
+{
+    type Response = S::Response;
+    type Error = BoxError;
+    type Future = ResponseFuture<S::Future>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        //
+        self.inner.poll_ready(cx).map_err(Into::into)
+    }
+
+    fn call(&mut self, request: Request) -> Self::Future {
+        let response_future = self.inner.call(request);
+
+        let sleep = tokio::time::sleep(self.timeout);
+        ResponseFuture {
+            response_future,
+            sleep,
+        }
+    }
+}
 
 fn init_app() -> Router {
     /*
@@ -51,9 +143,9 @@ async fn main() {
             governor_limiter.retain_recent();
         }
     });
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
-    let app = init_app().layer(GovernorLayer::new(governor_conf));
+    let app = init_app().layer(ServiceBuilder::new().layer(GovernorLayer::new(governor_conf)));
     let listener = TcpListener::bind(addr).await.unwrap();
     axum::serve(
         listener,
